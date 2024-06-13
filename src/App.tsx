af05a2c7e7090a9589f431d1a5b7081f85fcc995
Júlia Lima <juliafclima@hotmail.com>
2024-06-13 15:51:26 -0300

    Contexto entre rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import { ThemeProvider } from 'styled-components'
-import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from "styled-components";
+import { BrowserRouter } from "react-router-dom";
 
-import { Router } from './routes/Router'
+import { Router } from "./routes/Router";
 
-import { GlobalStyle } from './styles/global'
-import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+import { CyclesContextProvider } from "./contexts/CyclesContext";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
       </BrowserRouter>
-       
+
       <GlobalStyle />
     </ThemeProvider>
-  )
+  );
 }
