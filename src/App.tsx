12151d4b42301653fc9b7399acc588a4062b37dd
Júlia Lima <juliafclima@hotmail.com>
2024-06-13 14:17:46 -0300

    Entendendo contextos

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from 'styled-components'
 import { BrowserRouter } from 'react-router-dom'
-import { Router } from './Router'
+import { Router } from './routes/Router'
 
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
